b47ed33999f5765735e3b7360d76f7b102088fc9
vijay mysore <vmysore68@gmail.com>
2015-10-12 18:09:28 -0500

    rest working

--- api/health.php
+++ api/health.php
@@ -6,9 +6,16 @@
  * Time: 5:44 PM
  */
 
-class api_health extends \Tagged\Rest\Api\Base {
+class api_health {
 
     public function __construct() {
         echo "inside the api";
     }
+
+    public static function find() {
+	$response = new stdClass();
+	$response->id = 1;
+	$response->name = "vijay";
+	return $response;
+    }	
 }
