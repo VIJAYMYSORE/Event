acbed0a361fc390f734d0bdcc0b0f42b79f25750
vmysore <tagged2015>
2015-11-15 21:24:34 -0800

    wip

--- rest/rest.php
+++ rest/rest.php
@@ -7,6 +7,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $host = $_SERVER['SERVER_ADDR'];
  var_dump($_SERVER);
  var_dump($_REQUEST);
+ var_dump($_POST);
 
 $handlerMap = array(
     "/api/health|GET"=>"api_health|find",
