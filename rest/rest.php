d236ece7c78c25a61e1d0419f5227738aa975dc6
vmysore <tagged2015>
2015-11-15 18:37:16 -0800

    fixing the query parameter

--- rest/rest.php
+++ rest/rest.php
@@ -5,18 +5,21 @@ require_once( '../conf/config.php');
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 $host = $_SERVER['SERVER_ADDR'];
-var_dump($_SERVER);
-var_dump($_REQUEST);
+// var_dump($_SERVER);
+// var_dump($_REQUEST);
 
 $handlerMap = array(
     "/api/health|GET"=>"api_health|find",
 
 );
-
+// temp hack to fix the query parameter problem
+$request = explode("?", $uri);
+$uri = $request[0];
 $request = explode("|",$handlerMap[$uri."|".$method]);
 
 $response = call_user_func(array($request[0], $request[1]));
-var_dump($response);
+echo json_encode($response);
+// var_dump($response);
 
 //require_once $_SERVER['DocRoot'] . "/WebServices/{$request[0]}.php";
 
